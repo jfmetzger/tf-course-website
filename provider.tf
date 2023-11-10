eee881521cfad2b32d8c87a487f81931806aa1c4
jfmetzger <jfmetzger65@gmail.com>
2023-11-10 18:13:40 +0000

    Initial commit

---
+++ provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.41.0"
+    }
+  }
+  backend "gcs" {
+    bucket  = "roidtc-projects-209-tf-state"
+    prefix  = "terraform/state"
+  }
+}
+
+
+provider "google" {
+
+  project = var.project_id
+
+}
